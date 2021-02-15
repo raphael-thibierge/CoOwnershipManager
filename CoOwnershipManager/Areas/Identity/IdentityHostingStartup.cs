5747a6f5f915ed3e75d674b5da68ee6533a97b4a
Raphael THIBIERGE <raphael@thibierge.dev>
2021-02-15 22:31:10 +0100

    Scaffold Identity : generate view files

---
+++ CoOwnershipManager/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using CoOwnershipManager.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoOwnershipManager.Areas.Identity.IdentityHostingStartup))]
+namespace CoOwnershipManager.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
